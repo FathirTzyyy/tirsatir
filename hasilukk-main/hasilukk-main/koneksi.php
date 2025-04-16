a0f31b7e7ff64317232d16897dd1f65244b0d70d
Fathir29 <mhafat990@gmail.com>
2025-04-16 18:56:29 +0700

    Delete hasilukk-main/hasilukk-main directory

--- hasilukk-main/hasilukk-main/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_kasirukk");
-
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "koneksi database gagal : " . mysqli_connect_error();
-}
-
-?>
